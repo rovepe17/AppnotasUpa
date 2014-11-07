1050ee7ca78dee278daca4f600dfdcf006c50b22
rovepe17 <rociounipamplona@hotmail.com>
2014-11-07 18:39:34 -0500

    AppnotasUpa

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace AppnotasUpa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nota1 = new System.Windows.Forms.Label();
+            this.nota2 = new System.Windows.Forms.Label();
+            this.nota3 = new System.Windows.Forms.Label();
+            this.nota4 = new System.Windows.Forms.Label();
+            this.nota5 = new System.Windows.Forms.Label();
+            this.nota6 = new System.Windows.Forms.Label();
+            this.textNota151 = new System.Windows.Forms.TextBox();
+            this.textNota201 = new System.Windows.Forms.TextBox();
+            this.textNota152 = new System.Windows.Forms.TextBox();
+            this.textNota202 = new System.Windows.Forms.TextBox();
+            this.textNota103 = new System.Windows.Forms.TextBox();
+            this.textNota203 = new System.Windows.Forms.TextBox();
+            this.definitiva = new System.Windows.Forms.Button();
+            this.nuevo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nota1
+            // 
+            this.nota1.AutoSize = true;
+            this.nota1.Location = new System.Drawing.Point(41, 56);
+            this.nota1.Name = "nota1";
+            this.nota1.Size = new System.Drawing.Size(40, 13);
+            this.nota1.TabIndex = 0;
+            this.nota1.Text = "nota15";
+            // 
+            // nota2
+            // 
+            this.nota2.AutoSize = true;
+            this.nota2.Location = new System.Drawing.Point(41, 85);
+            this.nota2.Name = "nota2";
+            this.nota2.Size = new System.Drawing.Size(40, 13);
+            this.nota2.TabIndex = 1;
+            this.nota2.Text = "nota20";
+            // 
+            // nota3
+            // 
+            this.nota3.AutoSize = true;
+            this.nota3.Location = new System.Drawing.Point(41, 119);
+            this.nota3.Name = "nota3";
+            this.nota3.Size = new System.Drawing.Size(40, 13);
+            this.nota3.TabIndex = 2;
+            this.nota3.Text = "nota15";
+            // 
+            // nota4
+            // 
+            this.nota4.AutoSize = true;
+            this.nota4.Location = new System.Drawing.Point(41, 150);
+            this.nota4.Name = "nota4";
+            this.nota4.Size = new System.Drawing.Size(40, 13);
+            this.nota4.TabIndex = 3;
+            this.nota4.Text = "nota20";
+            // 
+            // nota5
+            // 
+            this.nota5.AutoSize = true;
+            this.nota5.Location = new System.Drawing.Point(41, 180);
+            this.nota5.Name = "nota5";
+            this.nota5.Size = new System.Drawing.Size(40, 13);
+            this.nota5.TabIndex = 4;
+            this.nota5.Text = "nota10";
+            // 
+            // nota6
+            // 
+            this.nota6.AutoSize = true;
+            this.nota6.Location = new System.Drawing.Point(41, 210);
+            this.nota6.Name = "nota6";
+            this.nota6.Size = new System.Drawing.Size(40, 13);
+            this.nota6.TabIndex = 5;
+            this.nota6.Text = "nota20";
+            // 
+            // textNota151
+            // 
+            this.textNota151.Location = new System.Drawing.Point(110, 56);
+            this.textNota151.Name = "textNota151";
+            this.textNota151.Size = new System.Drawing.Size(100, 20);
+            this.textNota151.TabIndex = 7;
+            // 
+            // textNota201
+            // 
+            this.textNota201.Location = new System.Drawing.Point(110, 85);
+            this.textNota201.Name = "textNota201";
+            this.textNota201.Size = new System.Drawing.Size(100, 20);
+            this.textNota201.TabIndex = 8;
+            // 
+            // textNota152
+            // 
+            this.textNota152.Location = new System.Drawing.Point(110, 119);
+            this.textNota152.Name = "textNota152";
+            this.textNota152.Size = new System.Drawing.Size(100, 20);
+            this.textNota152.TabIndex = 9;
+            // 
+            // textNota202
+            // 
+            this.textNota202.Location = new System.Drawing.Point(110, 150);
+            this.textNota202.Name = "textNota202";
+            this.textNota202.Size = new System.Drawing.Size(100, 20);
+            this.textNota202.TabIndex = 10;
+            // 
+            // textNota103
+            // 
+            this.textNota103.Location = new System.Drawing.Point(110, 173);
+            this.textNota103.Name = "textNota103";
+            this.textNota103.Size = new System.Drawing.Size(100, 20);
+            this.textNota103.TabIndex = 11;
+            // 
+            // textNota203
+            // 
+            this.textNota203.Location = new System.Drawing.Point(110, 207);
+            this.textNota203.Name = "textNota203";
+            this.textNota203.Size = new System.Drawing.Size(100, 20);
+            this.textNota203.TabIndex = 12;
+            // 
+            // definitiva
+            // 
+            this.definitiva.Location = new System.Drawing.Point(119, 265);
+            this.definitiva.Name = "definitiva";
+            this.definitiva.Size = new System.Drawing.Size(75, 23);
+            this.definitiva.TabIndex = 13;
+            this.definitiva.Text = "calcular";
+            this.definitiva.UseVisualStyleBackColor = true;
+            this.definitiva.Click += new System.EventHandler(this.definitiva_Click);
+            // 
+            // nuevo
+            // 
+            this.nuevo.AutoSize = true;
+            this.nuevo.Location = new System.Drawing.Point(41, 242);
+            this.nuevo.Name = "nuevo";
+            this.nuevo.Size = new System.Drawing.Size(36, 13);
+            this.nuevo.TabIndex = 14;
+            this.nuevo.Text = "limpiar";
+            this.nuevo.Click += new System.EventHandler(this.nuevo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 354);
+            this.Controls.Add(this.nuevo);
+            this.Controls.Add(this.definitiva);
+            this.Controls.Add(this.textNota203);
+            this.Controls.Add(this.textNota103);
+            this.Controls.Add(this.textNota202);
+            this.Controls.Add(this.textNota152);
+            this.Controls.Add(this.textNota201);
+            this.Controls.Add(this.textNota151);
+            this.Controls.Add(this.nota6);
+            this.Controls.Add(this.nota5);
+            this.Controls.Add(this.nota4);
+            this.Controls.Add(this.nota3);
+            this.Controls.Add(this.nota2);
+            this.Controls.Add(this.nota1);
+            this.Name = "Form1";
+            this.Text = "NOTAS UPA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nota1;
+        private System.Windows.Forms.Label nota2;
+        private System.Windows.Forms.Label nota3;
+        private System.Windows.Forms.Label nota4;
+        private System.Windows.Forms.Label nota5;
+        private System.Windows.Forms.Label nota6;
+        private System.Windows.Forms.TextBox textNota151;
+        private System.Windows.Forms.TextBox textNota201;
+        private System.Windows.Forms.TextBox textNota152;
+        private System.Windows.Forms.TextBox textNota202;
+        private System.Windows.Forms.TextBox textNota103;
+        private System.Windows.Forms.TextBox textNota203;
+        private System.Windows.Forms.Button definitiva;
+        private System.Windows.Forms.Label nuevo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppnotasUpa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void definitiva_Click(object sender, EventArgs e)
+        {
+            double nota151, nota201, nota152, nota202, nota103, nota203, suma;
+            nota151 = double.Parse(txtNota151.Text);
+            nota201 = double.Parse(txtNota201.Text);
+            nota152 = double.Parse(txtNota152.Text);
+            nota202 = double.Parse(txtNota202.Text);
+            nota103 = double.Parse(txtNota103.Text);
+            nota203 = double.Parse(txtNota203.Text);
+            suma = (nota151 * 0.15) + (nota201 * 0.20) + (nota152 * 0.15) + (nota202 * 0.20) + (nota103 * 0.10) + (nota203 * 0.20);
+            Txtresultado.Text = sumaToString();
+
+        }
+
+        private void nuevo_Click(object sender, EventArgs e)
+        {
+            If (suma>=3
+           {
+                 MessageBox.Show(“Aprobado”)
+}
+             else
+{
+                MessageBox.Show(“No Aprobado”)
+}
+
+            txtNota151.Text=””;
+            txtNota201.Text=””;
+            txtNota152.Text=””;
+            txtNota202.Text=””;
+            txtNota153.Text=””;
+            txtNota203.Text=””;
+            txtResultado.Text=””;
+
+        }
+    }
+}
